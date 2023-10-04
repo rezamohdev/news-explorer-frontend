e1ed1efc8c43a75a0319abcd5a7f82fc49ffbdc1
rezamohdev <mohammadireza29@gmail.com>
2023-10-04 10:16:43 -0400

    NewsCard

--- src/components/NewsCard/NewsCard.js
+++ src/components/NewsCard/NewsCard.js
@@ -8,7 +8,7 @@ export default function NewsCard() {
             <div className='newscard__info'>
                 <span className='newscard__date'>November 4, 2020</span>
                 <h5 className='newscard__title'>Everyone Needs a Special 'Sit Spot' in Nature</h5>
-                <p className='newscard'>
+                <p className='newscard__desciprtion'>
                     Ever since I read Richard Louv's influential book, "Last Child in the Woods," the idea of having a special "sit spot" has stuck with me. This advice, which Louv attributes to nature educator Jon Young, is for both adults and children to find...
                 </p>
                 <span className='newscard__author'>treehugger</span>
