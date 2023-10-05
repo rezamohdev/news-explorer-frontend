55be982eda8a51545321765ceac9255b5abcdf5c
rezamohdev <mohammadireza29@gmail.com>
2023-10-05 13:00:37 -0400

    loading not found

--- src/components/Loading/Loading.js
+++ src/components/Loading/Loading.js
@@ -1,16 +1,31 @@
 import React, { useState, useEffect } from "react";
 import './Loading.css';
-
+const magPicture = require('../../images/not-found_v1.png');
 export default function Loading() {
     const [loading, setLoading] = useState(true);
+
+    const result = false;
 
     useEffect(() => {
         setLoading(true);
     }, []);
     return (
         <div className="loading">
-            <div className='loading__spinner'></div>
-            <span className="loading__text">Searching for news...</span>
+            {result == true ? (
+                <>
+
+                    <img className="loading__image" src={magPicture} alt="not found" />
+                    <h4 className="loading__title">Nothing found</h4>
+                    <span className="loading__text">Sorry, but nothing matched
+                        your search terms.</span>
+
+                </>
+            ) : (
+                <>
+                    <div className='loading__spinner'></div>
+                    <span className="loading__text">Searching for news...</span>
+                </>
+            )}
         </div>
     )
 }
