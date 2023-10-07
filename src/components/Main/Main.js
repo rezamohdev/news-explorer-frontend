de97b152f5f85c7ba1faf3a9b8c9b4216bac78f3
rezamohdev <mohammadireza29@gmail.com>
2023-10-07 19:22:16 -0400

    news

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './Main.css';
 import About from '../About/About';
 import NewsCardList from '../NewsCardList/NewsCardList';
+import SavedNews from '../SavedNews/SavedNews';
 
 export default function Main() {
     return (
         <div className='main'>
             <About />
             <NewsCardList />
+            <SavedNews />
         </div>
     )
 }
---
+++ src/components/SavedNews/SavedNews.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './SavedNews.css';
+import NewsCard from '../NewsCard/NewsCard';
+
+export default function SavedNews() {
+    return (
+        <div className='saved-news'>
+            <NewsCard />
+        </div>
+    )
+}
