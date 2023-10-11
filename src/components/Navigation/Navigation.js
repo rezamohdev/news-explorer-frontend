f2bd5470a5e7d5921be248bbf9de3bb8deac7a1e
rezamohdev <mohammadireza29@gmail.com>
2023-10-11 12:34:30 -0400

    navigation modify

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -8,7 +8,7 @@ function Navigation({ inSavedNews = false, isLoggedIn, handleOpenSigninModal }) 
             <div className="navbar__left-side"><span className={`navbar__title ${inSavedNews ? 'navbar__title_saved-news' : ''}`}>NewsExplorer</span></div>
             <div className="navbar__right-side">
                 <button className={`navbar__button ${inSavedNews ? 'navbar__button_saved-news' : ''} ${inSavedNews ? '' : 'navbar__button_active'}`}>Home</button>
-                <button className={`navbar__button ${inSavedNews ? 'navbar__button_saved-news' : ''} ${inSavedNews ? 'navbar__button_active_saved_news' : 'navbar__button_active'}`}>Saved Articles</button>
+                {isLoggedIn && (<button className={`navbar__button ${inSavedNews ? 'navbar__button_saved-news' : ''} ${inSavedNews ? 'navbar__button_active_saved_news' : 'navbar__button_active'}`}>Saved Articles</button>)}
                 {isLoggedIn == true ?
                     (
                         <button className='navbar__logout-button' >Reza</button>
