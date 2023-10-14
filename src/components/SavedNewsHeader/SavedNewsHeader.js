24d21860170fe4afec542106387285d5e42f17a5
rezamohdev <mohammadireza29@gmail.com>
2023-10-14 18:54:43 -0400

    mobile mneu

--- src/components/SavedNewsHeader/SavedNewsHeader.js
+++ src/components/SavedNewsHeader/SavedNewsHeader.js
@@ -1,11 +1,13 @@
 import React from "react";
 import './SavedNewsHeader.css';
 import Nvaigation from '../Navigation/Navigation';
+import MobileNavigation from "../../vendor/MobileNavigation/MobileNavigation";
 
 export default function SavedNewsHeader({ isLoggedIn, handleOpenSigninModal, inSavedNews }) {
     return (
         <div className="saved-news-header">
             <Nvaigation inSavedNews={inSavedNews} isLoggedIn={isLoggedIn} handleOpenSigninModal={handleOpenSigninModal} />
+            <MobileNavigation inSavedNews={inSavedNews} handleOpenSigninModal={handleOpenSigninModal} isLoggedIn={isLoggedIn} />
             <div className="saved-news-header__titlebar">
                 <span className="saved-news-header__title">Saved articles</span>
                 <h2 className="saved-news-header__news-count">Elise, you have 5 saved articles</h2>
